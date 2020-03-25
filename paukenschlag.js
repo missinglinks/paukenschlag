98cbe6d66d3bef3470494dedf34e580122afe8be
pmuehleder <peter.muehleder@yahoo.de>
2020-03-25 16:17:29 +0100

    more links with paukenschlag

--- paukenschlag.js
+++ paukenschlag.js
@@ -1,11 +1,19 @@
-for (headline of document.getElementsByClassName("pdb-bigteaser-item-teaser-breadcrumb-headline-title")) {
+for (const headline of document.getElementsByClassName("pdb-bigteaser-item-teaser-breadcrumb-headline-title-link")) {
     headline.textContent = "PAUKENSCHLAG: " + headline.textContent
 }
 
-for (headline of document.getElementsByClassName("pdb-article-teaser-breadcrumb-headline-title")) {
+for (const headline of document.getElementsByClassName("pdb-article-teaser-breadcrumb-headline-title")) {
     headline.textContent = "PAUKENSCHLAG: " + headline.textContent
 }
 
-for (headline of document.getElementsByClassName("pdb-teaser3-teaser-breadcrumb-headline-title-link")) {
+for (const headline of document.getElementsByClassName("pdb-teaser3-teaser-breadcrumb-headline-title-link")) {
+    headline.textContent = "PAUKENSCHLAG: " + headline.textContent
+}
+
+for (const headline of document.getElementsByClassName("pdb-articles4-teaser-breadcrumb-headline-title-link")) {
+    headline.textContent = "PAUKENSCHLAG: " + headline.textContent
+}
+
+for (const headline of document.getElementsByClassName("pdb-toptopics-teaser-breadcrumb-headline-title-link")) {
     headline.textContent = "PAUKENSCHLAG: " + headline.textContent
 }
